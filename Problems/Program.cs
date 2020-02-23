e7767ac330160bfbfcd5e62e9cbaca805f51e5c0
Sinawo Mngxuma <g16M4204@ict.ru.ac.za>
2020-02-23 20:23:35 +0200

    project, cs and sln files commit

--- Problems/Program.cs
+++ Problems/Program.cs
@@ -2,11 +2,22 @@
 
 namespace Problems
 {
-    class Program
+     public class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            string input = Console.ReadLine();
+            Console.WriteLine(Prifix(input));
+        }
+
+        Prifix(string input)
+        {
+            int length= input.Length();
+            int[] strings = input.Split();
+            int count = strings.Length();
+
+            return length + "," + count + input;
         }
     }
 }
